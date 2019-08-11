27a9e8bf0ba6770ee000b7ff67734044caf6c2ea
狼豪 <34006560+lhempire@users.noreply.github.com>
2019-08-11 14:50:30 +0800

    Update wp-install.php

--- wp-install.php
+++ wp-install.php
@@ -135,7 +135,7 @@ if( isset( $_POST['step'] ) )
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>WordPress &gt; 远程安装程序</title>
-<link rel="stylesheet" id="combined-css" href="//<?php echo GITHUB_USERNAME; ?>.github.io/<?php echo GITHUB_PROJECT; ?>/stylesheets/combined.css" type="text/css" media="all">
+<link rel="stylesheet" id="combined-css" href="//<?php echo GITHUB_USERNAME; ?>.github.io/<?php echo GITHUB_PROJECT; ?>/style.css" type="text/css" media="all">
 </head>
 <body class="wp-core-ui">
 <h1 id="logo"><a href="http://wordpress.org/">WordPress远程安装程序</a></h1>
@@ -497,14 +497,7 @@ switch( $step ){
 
 <script src="//code.jquery.com/jquery.min.js"></script>
 <script src="//<?php echo GITHUB_USERNAME; ?>.github.io/<?php echo GITHUB_PROJECT; ?>/javascripts/installer.js"></script>
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-238524-33');
-ga('send', 'pageview', 'step<?php echo $step; ?>');
-ga('send', 'event', 'step', '<?php echo $step; ?>');
-</script>
+
+
 </body>
 </html>
